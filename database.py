9aea4422e810aac7da63c9ee44f31725c1619d6d
Matt Levine <mtlevine0@gmail.com>
2016-08-20 21:40:00 +0000

    Initial JWT Auth integration.

--- database.py
+++ database.py
@@ -6,26 +6,15 @@ import properties
 myDB = pw.MySQLDatabase(properties.d["database"], host=os.getenv('IP', 'localhost'), 
     port=3306, user=properties.d["dbUser"], passwd=properties.d["dbPass"])
 
-class Image(pw.Model):
-    fileName = pw.CharField(max_length=255)
-    imageID = pw.FixedCharField(max_length=7, primary_key=True)
+class User(pw.Model):
+    username = pw.FixedCharField(max_length=255, primary_key=True)
+    password = pw.CharField(max_length=255)
     createdDate = pw.DateTimeField(default=datetime.datetime.now)
-    viewCount = pw.IntegerField(default=0)
-    
-    class Meta:
-        database = myDB
-        
-class Album(pw.Model):
-    albumName = pw.CharField(max_length=255)
-    albumID = pw.FixedCharField(max_length=7, primary_key=True)
-    createdDate = pw.DateTimeField(default=datetime.datetime.now)
-    viewCount = pw.IntegerField(default=0)
-    imageID = pw.ForeignKeyField(Image)
-    
+
     class Meta:
         database = myDB
 
 
 # when you're ready to start querying, remember to connect
 myDB.connect()
-myDB.create_tables([Image, Album], safe=True)
+myDB.create_tables([User], safe=True)
