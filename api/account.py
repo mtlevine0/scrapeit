b3c8d1caf7f68d498036d9bb561b55abc7ba5662
Zach Ison <ison031@gmail.com>
2016-08-25 01:43:09 +0000

    Actually adding route organization and a view for an individual scrape

--- api/account.py
+++ api/account.py
@@ -2,22 +2,44 @@ from flask import Blueprint, render_template
 
 account_api = Blueprint('account_api', __name__)
 
+
+#Account Summary 
 @account_api.route('api/account', methods=['GET'])
 def account(uid):
+
     return render_template('summary.html')
-    
+
+
+#Account Settings    
 @account_api.route('api/account/settings', methods=['GET','POST'])
 def settings(uid):
+    
     return render_template('settings.html')
-
-@account_api.route('api/account/scrape/new', methods=['GET'])
-def new_scrape(uid):
+    
+    
+#View Individual Scrape    
+@account_api.route('api/account/scrape/<scrape_id>', methods=['GET'])
+def get_scrape(scrape_id):
+    
     return render_template('scrape.html')
 
+
+#Create New Scrape
+@account_api.route('api/account/scrape/new', methods=['GET'])
+def new_scrape(uid):
+    
+    return render_template('scrape.html')
+
+
+#Publish Scrape
 @account_api.route('api/account/scrape/create', methods=['POST'])
 def create_scrape(uid):
+    
     return render_template('account_summary.html')
     
+    
+#Edit Scrape Parameters, Delete Scrape, etc.     
 @account_api.route('api/account/scrape/edit', methods=['POST'])
 def edit_scrape(uid):
+    
     return render_template('account_summary.html')
