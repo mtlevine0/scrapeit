e02aec8c659b8d8a7aa620a74938e7a30559095d
Matt Levine <mtlevine0@gmail.com>
2016-08-25 04:23:36 +0000

    Fixed Python3 issues with the way auth_decorator is imported to scrapeit.

--- api/account.py
+++ api/account.py
@@ -1,23 +1,25 @@
 from flask import Blueprint, render_template
+import auth_decorator
 
 account_api = Blueprint('account_api', __name__)
 
 @account_api.route('/account', methods=['GET'])
-def account(uid):
+@auth_decorator.login_required
+def account():
     return render_template('summary.html')
     
 @account_api.route('/account/settings', methods=['GET','POST'])
-def settings(uid):
+def settings():
     return render_template('settings.html')
 
 @account_api.route('/account/scrape/new', methods=['GET'])
-def new_scrape(uid):
+def new_scrape():
     return render_template('scrape.html')
 
 @account_api.route('/account/scrape/create', methods=['POST'])
-def create_scrape(uid):
+def create_scrape():
     return render_template('account_summary.html')
     
 @account_api.route('/account/scrape/edit', methods=['POST'])
-def edit_scrape(uid):
+def edit_scrape():
     return render_template('account_summary.html')
--- scrapeit.py
+++ scrapeit.py
@@ -1,5 +1,5 @@
 from flask import Flask, send_from_directory
-from api import auth
+from api import auth, account, auth_decorator
 import os
 import properties
 
@@ -14,4 +14,5 @@ PORT = int(os.getenv('PORT', properties.d["port"]))
 if __name__ == '__main__':
     
     app.register_blueprint(auth.auth_api)
+    app.register_blueprint(account.account_api)
     app.run(debug=properties.d["debug"], host=properties.d["host"], port=PORT)
