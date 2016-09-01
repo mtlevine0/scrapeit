f202d52e46f313404582db5d8037806acb69664e
Garret <rendar713@gmail.com>
2016-09-01 01:46:11 +0000

    Fixed page redirecting issue; Changed color scheme; Fixed page layout

--- static/js/app.js
+++ static/js/app.js
@@ -11,10 +11,10 @@ myApp.config(['$routeProvider',
                  templateUrl: '/static/partials/index.html',
              }).
              when('/scrape', {
-                 templateUrl: '/static/partials/scrape.html',
+                 templateUrl: '../static/partials/scrape.html',
              }).
              when('/settings', {
-                 templateUrl: '/static/partials/settings.html',
+                 templateUrl: '../static/partials/settings.html',
              }).
              when('/summary', {
                  templateUrl: '../static/partials/summary.html',
